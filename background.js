4d26ecbc73be658563936c3ddca6585ce133e110
amalia <amalia20220301@outlook.com>
2022-03-07 08:31:18 +0800

    context menus

--- background.js
+++ background.js
@@ -1,19 +1,24 @@
 let color = '#3aa757';
 
+function onClickHandler(info, tab) {
+    alert(tab);
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
 chrome.runtime.onInstalled.addListener(async () => {
-  chrome.storage.sync.set({ color },()=>{
-    chrome.browserAction.setBadgeText({text: 'new'});
-    chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          // 只有打开百度才显示pageAction
-          new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'baidu.com'}})
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
+    chrome.storage.sync.set({color}, () => {
+        chrome.browserAction.setBadgeText({text: 'new'});
+        chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    // 只有打开百度才显示pageAction
+                    new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'baidu.com'}})
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
 });
